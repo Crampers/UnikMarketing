6356e8b6ac8f5778d49c8933c95a9dab678588f5
Simon Bo Dall Mikkelsen <smi@unik.dk>
2019-01-11 09:26:18 +0100

    Add request resource

--- src/UnikMarketing.Api/Controllers/RequestController.cs
+++ src/UnikMarketing.Api/Controllers/RequestController.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UnikMarketing.Business;
 using UnikMarketing.Domain;
 
 namespace UnikMarketing.Api.Controllers
@@ -9,18 +9,25 @@ namespace UnikMarketing.Api.Controllers
     [Route("requests")]
     public class RequestController : Controller
     {
+        private readonly IRequestService _requestService;
+
+        public RequestController(IRequestService requestService)
+        {
+            _requestService = requestService;
+        }
+
         //GET /requests (Gets all requests)
         [HttpGet]
         public async Task<ActionResult<ICollection<Request>>> GetRequests()
         {
-            throw new NotImplementedException();
+            return Ok(await _requestService.GetAll());
         }
 
         //GET /requests/{id} (Get requests with id)
         [HttpGet("{id}")]
         public async Task<ActionResult<Request>> GetRequest(int id)
         {
-            throw new NotImplementedException();
+            return Ok(await _requestService.Get(id));
         }
     }
 }
