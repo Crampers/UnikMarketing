595af869c11734e42dcfdd30c78f237570a5c109
Simon Bo Dall Mikkelsen <smi@unik.dk>
2019-01-16 09:32:05 +0100

    Map Range correctly

--- src/UnikMarketing.Api/Mapping/DtoToDomainProfile.cs
+++ src/UnikMarketing.Api/Mapping/DtoToDomainProfile.cs
@@ -14,7 +14,13 @@ namespace UnikMarketing.Api.Mapping
                     options => options.NullSubstitute(new CriteriaDto())
                 );
             CreateMap<LocationDto, Location>();
-            CreateMap(typeof(RangeDto<>), typeof(Range<>));
+            // TODO: Consider whether construction should be moved to a factory
+            CreateMap<RangeDto<int>, Range<int>>()
+                .ConstructUsing(source => new Range<int>(source.Min, source.Max));
+            CreateMap<RangeDto<float>, Range<float>>()
+                .ConstructUsing(source => new Range<float>(source.Min, source.Max));
+            CreateMap<RangeDto<decimal>, Range<decimal>>()
+                .ConstructUsing(source => new Range<decimal>(source.Min, source.Max));
             CreateMap<CriteriaDto, Criteria>();
             CreateMap<RequestDto, Request>();
         }
--- src/UnikMarketing.Domain/Range.cs
+++ src/UnikMarketing.Domain/Range.cs
@@ -2,17 +2,12 @@
 {
     public class Range<T>
     {
-        public Range()
-        {
-            
-        }
         public Range(T min, T max)
         {
             Min = min;
             Max = max;
         }
-
-        public int Id { get; set; }
+        
         public T Min { get; }
         public T Max { get; }
     }
