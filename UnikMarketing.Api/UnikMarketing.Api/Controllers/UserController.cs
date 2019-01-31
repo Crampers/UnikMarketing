04c73cb123a3dbb9426c2c893c9e46f0a13d40db
Simon Bo Dall Mikkelsen <smi@unik.dk>
2019-01-31 09:08:15 +0100

    Remove Criteria sub-resource

--- UnikMarketing.Api/UnikMarketing.Api/Controllers/UserController.cs
+++ UnikMarketing.Api/UnikMarketing.Api/Controllers/UserController.cs
@@ -64,23 +64,6 @@ namespace UnikMarketing.Api.Controllers
             //return Ok(requestDtos);
         }
 
-        //GET /users/{id}/criteria (Gets a user's criteria)
-        [HttpGet("{id}/criteria")]
-        public async Task<ActionResult<Criteria>> GetUserCriteria(string id)
-        {
-            throw new NotImplementedException();
-            User user = null;
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            var criteriaDto = _mapper.Map<CriteriaDto>(user.Criteria);
-
-            return Ok(criteriaDto);
-        }
-
         //POST /users (Creates new users)
         [HttpPost]
         public async Task<ActionResult<UserDto>> CreateUser([FromBody] UserDto userDto)
@@ -131,24 +114,6 @@ namespace UnikMarketing.Api.Controllers
 
             return Ok(updatedUserDto);
         }
-
-        //TODO: Fix adding of Criteria
-        //PUT /users/{id}/criteria (Updates a user's criteria))
-        //[HttpPut("{id}/criteria")]
-        //public async Task<ActionResult<User>> UpdateUserCriteria(int id, CriteriaDto criteriaDto)
-        //{
-        //    var user = await _userRepository.Get(id);
-
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var criteria = _mapper.Map<Criteria>(criteriaDto);
-        //    var updatedCriteria = await _userRepository.UpdateCriteria(user, criteria);
-
-        //    return Ok(updatedCriteria);
-        //}
 
         //DELETE /users/{id} (Self explanatory)
 
