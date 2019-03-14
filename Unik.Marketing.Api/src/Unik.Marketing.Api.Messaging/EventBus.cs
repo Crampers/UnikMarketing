48fdf429c15d64aa81d07bfcbfca7a467c5813e9
Simon Bo Dall Mikkelsen <smi@unik.dk>
2019-03-14 15:13:53 +0100

    Implement EventBus

--- Unik.Marketing.Api/src/Unik.Marketing.Api.Messaging/EventBus.cs
+++ Unik.Marketing.Api/src/Unik.Marketing.Api.Messaging/EventBus.cs
@@ -1,12 +1,32 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Unik.Marketing.Api.Messaging
 {
     public class EventBus : IEventBus
     {
+        private readonly Dictionary<Type, List<IEventHandler<IEvent>>> _subscribers = new Dictionary<Type, List<IEventHandler<IEvent>>>();
+
+        public void Subscribe(Type eventType, IEventHandler<IEvent> eventHandler)
+        {
+            if (!_subscribers.ContainsKey(eventType))
+            {
+                _subscribers.Add(eventType, new List<IEventHandler<IEvent>>());
+            }
+
+            _subscribers[eventType].Add(eventHandler);
+        }
+
         public Task Publish(IEvent @event)
         {
-            throw new System.NotImplementedException();
+            if (_subscribers.TryGetValue(@event.GetType(), out var subscribers))
+            {
+                return Task.WhenAll(subscribers.Select(x => x.Handle(@event)));
+            }
+
+            return Task.FromResult(0);
         }
     }
 }
